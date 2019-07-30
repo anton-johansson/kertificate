ff06b9e2ecf471790cd8a20895eb80aa4b809812
Anton Johansson <antoon.johansson@gmail.com>
2019-07-30 23:52:19 +0200

    Remove redandant function

--- pkg/pki/pki.go
+++ pkg/pki/pki.go
@@ -7,9 +7,7 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"fmt"
 	"math/big"
-	"os"
 	"time"
 )
 
@@ -61,70 +59,6 @@ func (generator *KeyGenerator) CreateCommonAuthority(data Certificate) ([]byte, 
 	}
 
 	return privateKeyBytes, certificateBytes, nil
-}
-
-func (generator *KeyGenerator) GeneratePrivateKey() error {
-	//template := &x509.Certificate{}
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return err
-	}
-
-	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
-	block := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: privateKeyBytes,
-	}
-	if err := pem.Encode(os.Stdout, block); err != nil {
-		return err
-	}
-
-	publicKeyBytes := x509.MarshalPKCS1PublicKey(&privateKey.PublicKey)
-	block = &pem.Block{
-		Type:  "RSA PUBLIC KEY",
-		Bytes: publicKeyBytes,
-	}
-	if err := pem.Encode(os.Stdout, block); err != nil {
-		return err
-	}
-
-	now := time.Now()
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(1653),
-		Subject: pkix.Name{
-			Country:            []string{"SE"},
-			Province:           []string{"Västra Götalands län"},
-			Locality:           []string{"Borås"},
-			StreetAddress:      []string{"Arlagatan 2"},
-			PostalCode:         []string{"503 36"},
-			Organization:       []string{"Anton AB"},
-			OrganizationalUnit: []string{"Operations"},
-			CommonName:         "Anton CA",
-		},
-		NotBefore:             now,
-		NotAfter:              now.AddDate(0, 0, 365),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-	}
-
-	certificateBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &privateKey.PublicKey, privateKey)
-	if err != nil {
-		fmt.Println("lul?")
-		return err
-	}
-
-	block = &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: certificateBytes,
-	}
-	if err := pem.Encode(os.Stdout, block); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (generator *KeyGenerator) CertificateToPem(certificate ICertificate) (string, error) {
